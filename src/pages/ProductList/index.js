52ee14dc4342ac0d8fe82feafb92344c53e22cfd
TranHuynh1999 <57291457+TranHuynh1999@users.noreply.github.com>
2021-07-08 02:30:52 +0700

    pagination, list course of category

--- src/pages/ProductList/index.js
+++ src/pages/ProductList/index.js
@@ -1,15 +1,75 @@
 
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import Header from "../../component/header";
 import Footer from "../../component/footer";
+import CallAPI from "../../until/callAPI";
+import {toast} from "react-toastify";
+import Loader from "../../component/loader";
+import {Pagination, Select} from "antd";
+const { Option } = Select;
 const ProductList=()=>{
+    const [listProduct,setListProduct]=useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [pages,setPages]= useState(1);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [listCategory,setListCategory]=useState([]);
+    const [idCategory,setIdCategory]=useState('-1');
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const res = await CallAPI("GET", null, `/categories`);
+            if(res.status === 1) {
+                setListCategory(res.data);
+            }
+            else
+                toast.error("Something went wrong. Try later")
+        }
+        fetchData();
+    }, [])
+    useEffect(() => {
+        const fetchData = async () => {
+            let res=null;
+            if(idCategory==='-1')
+            {
+                res = await CallAPI("GET", null, `/courses?page=${currentPage}`);
+            }
+            else
+            {
+                res = await CallAPI("GET", null, `/courses?page=${currentPage}&category_id=${idCategory}`);
+            }
+            if(res.status === 1) {
+                setIsLoading(false);
+                setListProduct(res.data.courses);
+                setPages(res.data.totalPage);
+            }
+            else
+                toast.error("Something went wrong. Try later")
+        }
+        fetchData();
+    }, [currentPage,pages,idCategory])
+    console.log(listProduct);
+    const onChangeCurrentPage=(data)=>{
+        setCurrentPage(data)
+        return data;
+    }
+    function onChangeCategories(value) {
+        setIdCategory(value);
+        console.log(value)
+    }
+
+
+    if(isLoading) return (
+        <div style={{marginLeft:'200px'}}>
+            <Loader/>
+        </div>
+    )
     return (
         <React.Fragment>
             <Header/>
             <div className="App">
                 <section className="section-pagetop bg">
                     <div className="container">
-                        <h2 className="title-page">Category products</h2>
+                        <h2 className="title-page">Category courses </h2>
                     </div>
                 </section>
                 <section className="section-content padding-y">
@@ -182,12 +242,23 @@ const ProductList=()=>{
                                 <header className="border-bottom mb-4 pb-3">
                                     <div className="form-inline">
                                         <span className="mr-md-auto">32 Items found </span>
-                                        <select className="mr-2 form-control">
-                                            <option>Latest items</option>
-                                            <option>Trending</option>
-                                            <option>Most Popular</option>
-                                            <option>Cheapest</option>
-                                        </select>
+                                        <Select
+                                            showSearch
+                                            style={{ width: 200 }}
+                                            placeholder="Select a person"
+                                            optionFilterProp="children"
+                                            onChange={onChangeCategories}
+                                            onSearch
+                                        >
+                                            <Option  value='-1'>All Categories</Option>
+                                            {
+                                                listCategory.map((data,index)=>{
+                                                    return(
+                                                        <Option key={index} value={data.id}>{data.name}</Option>
+                                                    )
+                                                })
+                                            }
+                                        </Select>,
                                         <div className="btn-group">
                                             <a href="#" className="btn btn-outline-secondary" data-toggle="tooltip"
                                                title="List view">
@@ -199,42 +270,58 @@ const ProductList=()=>{
                                     </div>
                                 </header>
                                 <div className="row">
-                                    <div className="col-md-4">
-                                        <figure className="card card-product-grid">
-                                            <div className="img-wrap">
-                                                <span className="badge badge-danger"> NEW </span>
-                                                <img src="assets/images/items/1.jpg"/>
-                                                <a className="btn-overlay" href="#"><i
-                                                    className="fa fa-search-plus"></i> Quick view</a>
-                                            </div>
-                                            <figcaption className="info-wrap">
-                                                <div className="fix-height">
-                                                    <a href="#" className="title">Great item name goes here</a>
-                                                    <div className="price-wrap mt-2">
-                                                        <span className="price">$1280</span>
-                                                        <del className="price-old">$1980</del>
+                                        {
+                                            listProduct.length > 0 ?
+                                                listProduct.map((data, index) => {
+                                                    return (
+                                                        <div className="col-md-4" key={index}>
+                                                        <figure className="card card-product-grid">
+                                                            <div className="img-wrap">
+                                                                <span className="badge badge-danger"> NEW </span>
+                                                                <img src="assets/images/items/course.jpg"/>
+                                                                <a className="btn-overlay" href="#"><i
+                                                                    className="fa fa-search-plus"></i> Quick view</a>
+                                                            </div>
+                                                            <figcaption className="info-wrap">
+                                                                <div className="fix-height">
+                                                                    <a href="#" className="title">{data.course.name}</a>
+                                                                    <div className="price-wrap mt-2">
+                                                                        <span className="price">{data.course.promotion_price}</span>
+                                                                        <del className="price-old">{data.course.price}</del>
+                                                                    </div>
+                                                                </div>
+                                                                <a href="#" className="btn btn-block btn-primary">Add to
+                                                                    cart </a>
+                                                            </figcaption>
+                                                        </figure>
+                                                        </div>
+                                                    )
+                                                })
+                                                :
+                                                <>
+                                                    <div style={{
+                                                        textAlign: "center",
+                                                        backgroundColor: '#364d79',
+                                                        width: '100%',
+                                                        height: '260px',
+                                                        lineHeight: '260px',
+                                                        marginBottom:'20px'
+                                                    }} className="flex-row justify-content-center">
+                                                        <p style={{fontSize: 30, fontWeight: 350}}>There are no recently courses!</p>
                                                     </div>
-                                                </div>
-                                                <a href="#" className="btn btn-block btn-primary">Add to cart </a>
-                                            </figcaption>
-                                        </figure>
-                                    </div>
+                                                </>
+                                        }
+
                                 </div>
-                                <nav className="mt-4" aria-label="Page navigation sample">
-                                    <ul className="pagination">
-                                        <li className="page-item disabled"><a className="page-link" href="#">Previous</a>
-                                        </li>
-                                        <li className="page-item active"><a className="page-link" href="#">1</a></li>
-                                        <li className="page-item"><a className="page-link" href="#">2</a></li>
-                                        <li className="page-item"><a className="page-link" href="#">3</a></li>
-                                        <li className="page-item"><a className="page-link" href="#">Next</a></li>
-                                    </ul>
-                                </nav>
+                                <div style={{textAlign:"center"}}>
+                                    <Pagination  simple onChange={onChangeCurrentPage} total={pages*10} />
+                                </div>
+
                             </main>
                         </div>
                     </div>
                 </section>
-        </div>
+            </div>
             <Footer/>
         </React.Fragment>
 
