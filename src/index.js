98681a9fcf682c685cdaf6c557de59294753b711
long1712578 <pdlong578@gmail.com>
2021-07-11 17:45:25 +0700

    Add router user

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import "assets/scss/argon-dashboard-react.scss";
 import AdminLayout from "layouts/Admin.js";
 import AuthLayout from "layouts/Auth.js";
 
+
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <CssBaseline />
